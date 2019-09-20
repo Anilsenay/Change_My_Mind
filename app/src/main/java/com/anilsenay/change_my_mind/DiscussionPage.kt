cb38b181f919f0368029394a8f7ed9526b636004
anilsenay <anilsenay3@gmail.com>
2019-09-21 02:12:10 +0300

    Profile Page design finished

--- app/src/main/java/com/anilsenay/change_my_mind/DiscussionPage.kt
+++ app/src/main/java/com/anilsenay/change_my_mind/DiscussionPage.kt
@@ -2,10 +2,18 @@ package com.anilsenay.change_my_mind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_discussion_page.*
 
 class DiscussionPage : AppCompatActivity() {
+
+    var round1 = false //for testing layout visibilities
+    var round2 = false
+    var round3 = false
+    var round4 = false
+    var round5 = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +24,7 @@ class DiscussionPage : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         val mTitle = toolbar.findViewById(R.id.toolbar_title) as TextView
-        mTitle.setText("Profile")
+        mTitle.setText("Discussion")
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         toolbar.setNavigationIcon(R.drawable.back) //set icon
@@ -27,5 +35,21 @@ class DiscussionPage : AppCompatActivity() {
         }
         // --> end of toolbar
 
+        //checkRounds()
+
     }
+    //testing layout visibilities
+     /*
+    fun checkRounds(){
+        if(!round1)
+
+        if(!round2)
+            round2_layout.visibility = View.GONE
+        if(!round3)
+            round3_layout.visibility = View.GONE
+        if(!round4)
+            round4_layout.visibility = View.GONE
+        if(!round5)
+            round5_layout.visibility = View.GONE
+    }*/
 }
